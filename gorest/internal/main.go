e78e99391edfcf6c0522a909d8b34c86adb23fb4
Neeraj Ajithkumar <hello@neerajajithkumar.com>
2022-05-17 23:22:35 +0530

    Output Edit

--- gorest/internal/main.go
+++ gorest/internal/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Welcome to my Web Server!, %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Println("Listening on localhost:8080")
